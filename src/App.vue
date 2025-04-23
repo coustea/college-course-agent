72c493b10c9251e65ba16ceac134d1a20436c452
Cathy <2982893677@qq.com>
2025-04-23 10:46:28 +0800

    教师面板

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TeacherDashboard />
+</template>
+
+<script>
+import TeacherDashboard from './components/TeacherDashboard.vue';
+
+export default {
+  components: {
+    TeacherDashboard
+  }
+};
+</script>
