f14534eba6e1c0488eb7ce1f33b853ba32f96656
我最狠了 <16167388+im-the-toughest@user.noreply.gitee.com>
2025-09-07 16:44:59 +0800

    1

--- src/main/java/com/ccut/common/entity/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.ccut.common.entity;
-
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = true)
-public class Admin extends User{
-
-}
---
+++ src/main/java/com/ccut/entity/Teacher.java
@@ -0,0 +1,18 @@
+package com.ccut.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+// 有参构造函数
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher extends  User{
+    private String employeeNumber; // 员工编号
+    private String name;// 教师姓名
+    private String email;// 邮箱地址
+    private String phone; // 联系电话
+    private String department;// 所属部门
+    private String title;// 职称
+}
