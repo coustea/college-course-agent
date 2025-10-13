ab75494be4b459f8b087501517ffd49e532cd7aa
我最狠了 <16167388+im-the-toughest@user.noreply.gitee.com>
2025-10-13 17:19:43 +0800

    学生改作品

--- src/main/java/com/ccut/mapper/StudentSubmissionMapper.java
+++ src/main/java/com/ccut/mapper/StudentSubmissionMapper.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface StudentSubmissionMapper {
 
     int insert(StudentSubmission studentSubmission);
+    int updateById(StudentSubmission studentSubmission);
     StudentSubmission selectByGroupId(@Param("groupId") Long groupId);
     StudentSubmission selectByAssignmentIdAndGroupId(@Param("assignmentId") Long assignmentId, @Param("groupId") Long groupId);
     List<StudentSubmission> selectByAssignmentId(@Param("assignmentId") Long assignmentId);
